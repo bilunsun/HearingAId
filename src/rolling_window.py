7a84133390315d25ea0308f776f6c70422b7585a
Patrick Liu <patrick.liu08@gmail.com>
2022-03-02 12:51:06 -0500

    Implement low-pass filter and sending over I2C

--- src/rolling_window.py
+++ src/rolling_window.py
@@ -6,12 +6,17 @@ from collections import deque
 from threading import Event, Lock, Thread
 
 from train import Model
+from user_output import send_class
 
 
 CHANNELS = 1
 CHUNK = 1_600
 SAMPLE_RATE = 16_000
 WINDOW_TIME_S = 4
+
+CLASSIFY_RATE = 5  # classification rate in Hz
+CLASSIFY_PERIOD = 1 / CLASSIFY_RATE
+CLASSIFY_HIST_TIME = 2  # seconds of classifications to hold on to
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -48,6 +53,11 @@ classes = [
     "water",
 ]
 
+class_to_byte = {}
+
+for i, c in enumerate(classes):
+    class_to_byte[c] = i
+
 
 def yield_data(dq: deque, lock: Lock, exit_signal: Event):
     p = pyaudio.PyAudio()
@@ -62,6 +72,39 @@ def yield_data(dq: deque, lock: Lock, exit_signal: Event):
     p.terminate()
 
 
+def send_classifications():
+    """
+    Applies a low-pass filter to the classes detected, and sends the classification
+    """
+    prev_sent = 'silence'
+
+    while not exit_signal.is_set():
+        # dumb low pass filter
+        # look at the past history. If class persists for more than 1 second and is the most frequent
+        detected = list(detect_q)
+        freqs = {}
+
+        for c in detected:
+            if c in freqs:
+                freqs[c] += 1
+            else:
+                freqs[c] = 1
+
+        max_count = 0
+        max_class = None
+        for c in freqs:
+            if max_count < freqs[c]:
+                max_count = freqs[c]
+                max_class = c
+
+        if max_class != 'silence':
+            # send result
+            # send_class(class_to_byte[max_class])
+            print(max_class)
+
+        time.sleep(1)
+
+
 @torch.no_grad()
 def classify(x: deque):
     x = torch.frombuffer(b"".join(x), dtype=torch.int16)
@@ -72,7 +115,9 @@ def classify(x: deque):
     x = F.softmax(x, dim=1)
 
     max_index = torch.argmax(x, dim=1).item()
-    print(classes[max_index])
+    print(classes[max_index], x)
+    with detect_q_lock:
+        detect_q.append(classes[max_index])
 
 
 # Putting as globals to kill on exit
@@ -83,10 +128,18 @@ dq = deque(maxlen=buffer_len)
 exit_signal = Event()
 data_thread = Thread(target=yield_data, args=(dq, lock, exit_signal,))
 
+# Queue to store detection results
+detect_q_lock = Lock()
+detect_q_len = CLASSIFY_RATE * CLASSIFY_HIST_TIME
+detect_q = deque(["silence" for x in range(detect_q_len)], maxlen=detect_q_len)  # stuff full of silence
+send_thread = Thread(target=send_classifications)
+
 
 def main():
     data_thread.start()
+    send_thread.start()
 
+    last_classification = time.time()
     while True:
         raw_data = list(dq)  # The 'lock' object does not seem to be necessary for reading
 
@@ -94,7 +147,11 @@ def main():
             time.sleep(WINDOW_TIME_S)
             continue
 
-        classify(raw_data)
+        if time.time() - last_classification >= CLASSIFY_PERIOD:
+            classify(raw_data)
+            last_classification = time.time()
+        else:
+            pass
 
 
 if __name__ == "__main__":
